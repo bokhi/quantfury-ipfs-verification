1596cc93d5c0af91ee5ac5370311ae0a404dd803
Martin Boissier <martin.boissier@gmail.com>
2023-07-10 12:53:54 +0800

    WIP babashka implementation
    performance of aggregating the spread is terrible
    better simply use the bash script for now

--- src/quantfury_ipfs_verification/app.clj
+++ src/quantfury_ipfs_verification/app.clj
@@ -1,4 +1,74 @@
-(ns quantfury-ipfs-verification.app)
+(ns quantfury-ipfs-verification.app
+  (:require [babashka.process :as p]
+            [babashka.curl :as curl]
+            [clojure.edn :as edn]
+            [clojure.data.csv :as csv]
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
 
-(defn -main [& args]
-  (apply println "Hello from my namespace!" args))
+(def data-dir "data")
+(def ipfs-url "https://ipfs.quantfury.com/ipfs/")
+
+(defn calculate-results [csv-data]
+  (reduce (fn [{:keys [num-trades sum-spread]} row]
+            (let [spread (-> row (get 7) bigdec)]
+              {:num-trades (inc num-trades)
+               :sum-spread (+ sum-spread spread)}))
+          {:num-trades 0, :sum-spread 0M}
+          csv-data))
+
+(defn process-csv [csv-file]
+  (with-open [rdr (io/reader csv-file)]
+    (->> rdr
+         csv/read-csv
+         rest
+         calculate-results)))
+
+(defn download-cid [cid]
+  (let [url (str ipfs-url cid)
+        zip-file (io/file data-dir (str cid ".zip"))]
+    (when-not (.exists zip-file)
+      (io/copy (:body (curl/get url {:as :bytes})) zip-file))
+    zip-file))
+
+(defn decrypt-file [file password]
+  (let [temp-file (-> file
+                       .getName
+                       (str/split #"\.")
+                       first
+                       (java.io.File/createTempFile ".csv" ))
+        command   ["7z x -y -so" (str "-p" password) (.getPath file)]]
+    (.deleteOnExit temp-file)
+    (apply p/shell {:out :write :out-file temp-file} command)
+    temp-file))
+
+(defn process-cid-password [{:keys [cid password expected]}]
+  (let [zip-file (download-cid cid)
+        csv-file (decrypt-file zip-file password)
+        results (process-csv csv-file)]
+    (if (= expected results)
+      (println "OK: " cid)
+      (println "FAILED: " cid " Expected: " expected " Got: " results))))
+
+(defn -main []
+  (let [cids-passwords (edn/read-string (slurp "cids_passwords.edn"))]
+    (doseq [cp cids-passwords]
+      (process-cid-password cp))))
+
+(comment
+
+  (def temp-csv (-> "QmVB76oSA1GmxZWHseLiivrcfNE4ws2HhE5m7g491v8ENx"
+                download-cid
+                (decrypt-file "lb[732;.P!ULSMwbOEXF")))
+
+  (with-open [rdr (io/reader temp-csv)]
+    (->> rdr
+         csv/read-csv
+         rest
+         (take 1000)
+         calculate-results
+         time))
+
+  (.delete temp-csv)
+
+  )
